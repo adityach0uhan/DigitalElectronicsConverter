0753d091ee467eca5fff988f98e12520098bff57
adityach0uhan <m4with6x@gmail.com>
2023-10-19 23:25:30 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import numberSystemsData from '../public/numberSystem.js';
+import cards from '@/components/cards.jsx';
 export default function Home() {
 
   return (
     <>
 
-      <div className="h-full w-full bg-blue-200 my-5 flex ">
+      <div className="h-full w-full bg-blue-200 my-5  ">
+        hello Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id, necessitatibus ab asperiores, sint eligendi voluptas, corporis tempora eius exercitationem unde consequuntur eveniet. In facere officia accusamus tempora, minima ab dolorum facilis adipisci.
 
-        {numberSystemsData.number_systems.map((item) => {
-          <div><p>{item.name}</p></div>
-        })}
+        {
+          numberSystemsData.number_systems.map((item) => {
+           return <cards item={item} />
+          })
+        }
 
       </div>
     </>
---
+++ components/cards.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const cards = (item) => {
+  return (
+      <div className='w-20 h-40 bg-blue-200'>
+          <div><h3>Heading </h3></div>
+          <div>description</div>
+    </div>
+  )
+}
+
+export default cards
