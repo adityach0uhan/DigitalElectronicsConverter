0fb510dd4088500d49c4658b7e6f2232c02b44bf
adityach0uhan <m4with6x@gmail.com>
2023-10-27 22:42:36 +0530

    Added routes

---
+++ app/Binary/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import Binarypage from './Binarypage'
+
+const page = () => {
+  return (
+    <div>This is the Binary Route Page
+      {/* <Binarypage/> */}
+    </div>
+  )
+}
+
+export default page
--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import numberSystemsData from '../public/numberSystem.js';
+import Link from 'next/link.js';
 import Binarypage from './Binary/Binarypage.jsx';
 
 export default function Home() {
@@ -29,14 +30,22 @@ export default function Home() {
 
   return (
     <>
-      <div className="h-full w-full  px-2 py-2 flex flex-wrap">
-        {BinaryData.length !== 0 ? (
+      <div className='flex flex-row bg-black-900 flex-wrap'>
+        <div className='m-auto text-white text-l md:text-2xl font-extrabold my-10 text-bold mb-4'>Number System Conversions</div>
+        <div className=" w-full justify-center py-1 align-center flex flex-wrap">
+
+          <Link href="/Binary"><div className='centertheText w-32 h-40 text-center md:w-60 hover:bg-violet-600 my-2 mx-2 bg-blue-400 rounded'>Binary System Conversions</div></Link>
+          <Link href="/Binary"><div className='centertheText w-32 h-40 text-center md:w-60 hover:bg-violet-600 my-2 mx-2 bg-blue-400 rounded'>Decimal System Conversions</div></Link>
+          <Link href="/Binary"><div className='centertheText w-32 h-40 text-center md:w-60 hover:bg-violet-600 my-2 mx-2 bg-blue-400 rounded'>Octal System Conversions</div></Link>
+          <Link href="/Binary"><div className='centertheText w-32 h-40 text-center md:w-60 hover:bg-violet-600 my-2 mx-2 bg-blue-400 rounded'>HexaDecimal System Conversions</div></Link>
+          {/* {BinaryData.length !== 0 ? (
           <Binarypage binaryData={BinaryData} /> 
         ) : (
           "Binary Component is loading"
-        )}
+        )} */}
 
-        
+
+        </div>
       </div>
     </>
 
