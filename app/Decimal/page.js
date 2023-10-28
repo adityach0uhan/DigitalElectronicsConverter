75ac2ebdff13a701b1d722b7a7169d9a0d6db2a4
adityach0uhan <m4with6x@gmail.com>
2023-10-28 23:14:03 +0530

    Build Failed fixed

--- app/Decimal/page.js
+++ app/Decimal/page.js
@@ -1,25 +1,25 @@
-"use client"
+// "use client"
 import React from 'react';
 // import { useState, useEffect } from 'react';
 // import numberSystemsData from '../public/numberSystem.js';
 
 const Page = () => {
-//   const [DecimalData, setDecimalData] = useState([])
-//   useEffect(() => {
-//     async function fetchData() {
-//       try {
-//         setDecimalData(numberSystemsData.number_systems[1])
-//       } catch (error) {
-//         console.error('Error fetching file data:', error);
-//       }
-//     }
-//     fetchData();
-//   }, [''])
-  return (
-    <div className='text-white text-center'>This is the Decimal Route Page
+    //   const [DecimalData, setDecimalData] = useState([])
+    //   useEffect(() => {
+    //     async function fetchData() {
+    //       try {
+    //         setDecimalData(numberSystemsData.number_systems[1])
+    //       } catch (error) {
+    //         console.error('Error fetching file data:', error);
+    //       }
+    //     }
+    //     fetchData();
+    //   }, [''])
+    return (
+        <div className='text-white text-center'>This is the Decimal Route Page
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Page
