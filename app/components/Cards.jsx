9359cb62200543f5ecbbe36aeaeac6586b1bd7e1
adityach0uhan <m4with6x@gmail.com>
2024-01-09 21:27:05 +0530

    Back To Normal

--- app/components/Cards.jsx
+++ app/components/Cards.jsx
@@ -7,7 +7,8 @@ const Cards = ({ data, index, NumSys }) => {
     <Link href={`/${NumSys}/Operations/${index+1}`}>
     < div className='centertheText w-52 h-40 flex flex-col mx-5 mb-5 my-5 text-center border-solid border-2 border-sky-700 ' >
       <h3 className='text-xl text-black my-4'>{data.operation}</h3>
-      <p className='text-black text-sm p-1'>{data.description}</p>
+        <p className='text-black text-sm p-1'>{data.description}</p>
+        <div></div>
       </div >
     </Link>
   )
